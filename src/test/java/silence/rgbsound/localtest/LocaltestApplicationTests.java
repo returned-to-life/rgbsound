ee01c8d45fae0855b8bff011da6065ebb81e598a
Asia <asia@example.net>
2020-02-13 11:45:38 +0300

    configuration files

---
+++ src/test/java/silence/rgbsound/localtest/LocaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package silence.rgbsound.localtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
